aa6b2cd73154615bd720ce425b1ae79d189fbf0b
fgamester <falvarez.munoz@hotmail.es>
2024-11-25 20:08:53 -0300

    basic line animation added

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -18,12 +18,12 @@ const Board = ({ turn, changeTurn, isOver, winner, setWinner, playing }) => {
             [0, 4, 8],
             [2, 4, 6]
         ];
-        for (let line of lines) {
-            const [a, b, c] = line;
+        for (let l of lines) {
+            const [a, b, c] = l;
             if (cells[a] !== null && (cells[a] === cells[b] && cells[a] === cells[c])) {
                 isOver(_ => false);
                 setWinner(_ => true);
-                setWinnerLine(_ => lines.indexOf(line) + 1);
+                setWinnerLine(_ => lines.indexOf(l) + 1);
                 setTimeout(_ => { setTransition(_ => true) }, 500);
                 break;
             }
@@ -40,20 +40,20 @@ const Board = ({ turn, changeTurn, isOver, winner, setWinner, playing }) => {
     }
 
     const calculateWidth = (measure) => {
+        const diagonal = (winnerLine == 7 || winnerLine == 8);
         if (measure == 'vw') {
-            if (window.innerWidth > 500) {
-                return window.innerWidth * 0.2 * 2.5;
-            } else {
-                return 250;
-            }
+            if (window.innerWidth >= 500 && diagonal) return window.innerWidth * 0.2 * 3.2;
+            if (window.innerWidth < 500 && diagonal) return 320;
+            if (window.innerWidth > 500) return window.innerWidth * 0.2 * 2.4;
+            return 250;
         } else {
-            if (window.innerWidth > 300) {
-                return window.innerHeight * 0.2 * 2.5
-            } else {
-                return 250;
-            }
+            if (window.innerHeight >= 500 && diagonal) return window.innerHeight * 0.2 * 3.2;
+            if (window.innerHeight < 500 && diagonal) return 320;
+            if (window.innerHeight > 500) return window.innerHeight * 0.2 * 2.4;
+            return 250;
         }
     }
+
 
     const setCellSize = () => {
         const cellSize = ((window.innerHeight < window.innerWidth) ? '20vh' : '20vw');
@@ -61,14 +61,14 @@ const Board = ({ turn, changeTurn, isOver, winner, setWinner, playing }) => {
     }
 
     const setPaintedSize = () => {
-        const cellSize = ((document.documentElement.style.getPropertyValue('--cell-size')))
-        let value = ''
-        let measure = ''
+        const cellSize = ((document.documentElement.style.getPropertyValue('--cell-size')));
+        let value = '';
+        let measure = '';
         for (let i = 0; i < cellSize.length - 2; i++) value += cellSize[i];
         for (let i = cellSize.length - 2; i < cellSize.length; i++) measure += cellSize[i];
         const fontSize = calculateSize(measure);
         const inString = fontSize > 50 ? `${Math.round(fontSize)}px` : '50px';
-        document.documentElement.style.setProperty('--font-size', inString)
+        document.documentElement.style.setProperty('--font-size', inString);
     }
 
     const setLineWidth = () => {
@@ -87,16 +87,50 @@ const Board = ({ turn, changeTurn, isOver, winner, setWinner, playing }) => {
         } else if (w == 7) {
             document.documentElement.style.setProperty('--line-rotation', '45deg');
             console.log('45°')
-        } else if (w==8){
-            document.documentElement.style.setProperty('--line-rotation', '135deg');
+        } else if (w == 8) {
+            document.documentElement.style.setProperty('--line-rotation', '-45deg');
             console.log('135°')
         }
     }
 
+    const verifyMeasure = (measure) => {
+        if (measure == 'vw' && window.innerWidth >= 500) return true;
+        if (measure == 'vh' && window.innerHeight >= 500) return true;
+        return false;
+    }
+
+
+    const setPosition = () => {
+        const cellSize = ((document.documentElement.style.getPropertyValue('--cell-size')))
+        const measure = `${cellSize[cellSize.length - 2]}${cellSize[cellSize.length - 1]}`;
+        let position = '';
+        if (winnerLine == 1) {
+            position = `-100px`
+            if (verifyMeasure(measure)) position = `-${cellSize}`;
+            document.documentElement.style.setProperty('--y-translation', position);
+        }
+        if (winnerLine == 3) {
+            position = `100px`
+            if (verifyMeasure(measure)) position = `${cellSize}`;
+            document.documentElement.style.setProperty('--y-translation', position);
+        }
+        if (winnerLine == 4) {
+            position = `-100px`
+            if (verifyMeasure(measure)) position = `-${cellSize}`;
+            document.documentElement.style.setProperty('--x-translation', position);
+        }
+        if (winnerLine == 6) {
+            position = `100px`
+            if (verifyMeasure(measure)) position = `${cellSize}`;
+            document.documentElement.style.setProperty('--x-translation', position);
+        }
+    }
+
     const setProperties = () => {
-        setPaintedSize();
         setCellSize();
         setLineWidth();
+        setPaintedSize();
+        setPosition();
     }
 
     useEffect(() => {
@@ -115,6 +149,8 @@ const Board = ({ turn, changeTurn, isOver, winner, setWinner, playing }) => {
 
     useEffect(() => {
         setDirection();
+        setLineWidth();
+        setPosition();
     }, [winnerLine])
 
     return (
@@ -123,8 +159,12 @@ const Board = ({ turn, changeTurn, isOver, winner, setWinner, playing }) => {
                 {cells.map((item, index) => (
                     <Box key={index} item={item} index={index} play={setCells} list={cells} turn={turn} changeTurn={changeTurn} checkWinner={checkWinner} playing={playing} />
                 ))}
+                {winner && (
+                    <div className="line-box winner">
+                        <hr className={`winner-line ${transition && 'winner'}`} />
+                    </div>
+                )}
             </div>
-            {winner && <hr className={`winner-line ${transition && 'winner'}`} />}
         </>
     )
 }
