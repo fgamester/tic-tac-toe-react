53dc79a65fe6cb270b846b9581800c7e3a8641cf
fgamester <falvarez.munoz@hotmail.es>
2024-11-30 22:13:25 -0300

    animation for modal finished, css var moved to index.css

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,30 +1,35 @@
 import { useEffect, useState } from "react";
 import Board from "./board";
-import '../styles/game.css'
+import '../styles/game.css';
 
 const Game = () => {
     const [inMenu, setInMenu] = useState(false);
     const [gameTurn, setGameTurn] = useState(true);
     const [winner, setWinner] = useState(false);
     const [playing, setPlaying] = useState(true);
-    const [transition, setTransition] = useState(false);
+    const [transition, setTransition] = useState([false, false]);
 
     const gameOver = () => {
         if (!playing) {
-            setTimeout(_ => setTransition(_ => true), 500);
+            setTimeout(_ => setTransition(prev => [true, prev[1]]), 500);
+            setTimeout(_ => setTransition(prev => [prev[0], true]), 1000);
         }
     }
 
     useEffect(() => {
         gameOver();
-    }, [playing])
+    }, [playing]);
 
 
     return (
         <div className="game">
             <Board turn={gameTurn} changeTurn={setGameTurn} setWinner={setWinner} winner={winner} setPlaying={setPlaying} playing={playing} />
             {!playing && !inMenu && (
-                <div className={`end-game ${!playing && transition && 'game-over'}`}></div>
+                <div className={`end-game ${!playing && transition[0] && 'game-over'}`}>
+                    <div className={`modal ${transition[1] && 'modal-transform'}`}>
+                        GAME OVER
+                    </div>
+                </div>
             )}
         </div>
     )
