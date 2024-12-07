bc6d74d53640863c29546657b5723f3a6b4bcce1
fgamester <falvarez.munoz@hotmail.es>
2024-12-07 12:07:18 -0300

    more breakpoints added

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,10 +1,10 @@
 const Modal = ({ winner, restart, turn, players }) => {
     return (
-        <div className='modal'>
+        <div id="modal">
             <h1 className="game-over">GAME OVER</h1>
             <h3 className={`winner-message ${winner && 'remove-margin'}`}>{winner ? 'The Winner is' : 'There is no winner this time'}</h3>
-            <h2>{turn ? players[0] : players[1]}</h2>
-            {winner && <h1 className="winner-player">{turn ? 'O' : 'X'}</h1>}
+            {winner && <h2>{turn ? players[0] : players[1]}</h2>}
+            {winner && <h1 id="winner-player">{turn ? 'O' : 'X'}</h1>}
             <button onClick={restart} className="btn-restart">Play again</button>
         </div>
     );
